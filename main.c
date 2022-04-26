0f7233fadb687633d32374b23f28f7d46fc50c1b
Séverin Konishi <konishi.seve@gmail.com>
2022-04-26 10:05:39 +0200

    Mise au propre
    suppression de pi_regulator et mise au propre du main.c et du process_image.c, relecture peut être utile

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
-#include <usbcfg.h>
 #include <main.h>
 #include <motors.h>
 #include <camera/po8030.h>
 #include <chprintf.h>
 
-#include <pi_regulator.h>
 #include <process_image.h>
 #include "ch.h"
 #include "hal.h"
@@ -20,17 +18,13 @@
 #include <leds.h>
 #include "spi_comm.h"
 
+// Pour faire marcher les capteurs de proximité
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
 
+//Utilisé pour le débug
 static void serial_start(void)
 {
 	static SerialConfig ser_cfg = {
@@ -43,78 +37,38 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
+
 int main(void)
 {
+	//configuration du microcontrolleur
 	halInit();
 	chSysInit();
 	mpu_init();
 
-	serial_start();
-	usb_start();
 
-	spi_comm_start();
+	serial_start(); //Pour le débug
 
+	spi_comm_start(); //Pour les leds rgb
+
+	//configuration de la caméra
 	dcmi_start();
 	po8030_start();
+
+	//configuration des moteurs
 	motors_init();
-    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    messagebus_init(&bus, &bus_lock, &bus_condvar); // ????????????????
 
+    //initialisation des capteurs de proximité
 	proximity_start();
-
 	messagebus_topic_t *proximity_topic = messagebus_find_topic_blocking(&bus, "/proximity");
 	proximity_msg_t prox;
-	//pi_regulator_start();
+
+
 	process_image_start();
 	chThdSleepMilliseconds(100);
-	calibrate_ir();
-	/*set_led(1,1);
-	set_body_led(1);
-	set_rgb_led(LED4, 99, 0, 0);
-	set_rgb_led(LED6, 0,99,0);
-	set_rgb_led(LED8,0,0,30);*/
 
-	//int16_t speedi = 200;
-	//int16_t speedi2 = 200;
-	int8_t Kp = 1;
-	int16_t speedk = 200;
-	int16_t speedk2 = 200;
 	while(1){
 		chThdSleepMilliseconds(200);
-		/*chprintf((BaseSequentialStream *)&SDU1, "calib = %x \n", get_calibrated_prox(1));
-		//chprintf((BaseSequentialStream *)&SDU1, "prox = %x \n", get_prox(1));
-		chprintf((BaseSequentialStream *)&SDU1, "diff = %x \n", get_ambient_light(0));
-		chprintf((BaseSequentialStream *)&SDU1, "amb1 = %x \n", get_ambient_light(1));
-		chprintf((BaseSequentialStream *)&SDU1, "amb2 = %x \n", get_ambient_light(2));
-		set_rgb_led(2, 0, 99, 0);*/
-		//chprintf((BaseSequentialStream *)&SDU1, "proxright = %d ", get_prox(0));
-		//chprintf((BaseSequentialStream *)&SDU1, "proxleft = %d ", get_prox(7));
-
-		int16_t proxLeft = get_prox(7) + get_prox(6);
-		int16_t proxRight = get_prox(0) + get_prox(1);
-
-		int16_t diff = proxLeft - proxRight;
-		speedk = diff * Kp;
-		speedk2 =  - diff * Kp;
-		/*if(proxLeft - proxRight > 100){
-			if(speedi<1000){
-				speedi += 100;
-			}
-			if(speedi2 > -1000){
-				speedi2 -= 100;
-			}
-		} else if(proxRight - proxLeft > 100){
-			if(speedi2<1000){
-				speedi2 += 100;
-			}
-			if(speedi > -1000){
-				speedi -= 100;
-			}
-		} else {
-			speedi = 200;
-			speed2i = 200;
-		}*/
-		left_motor_set_speed(200 + speedk);
-		right_motor_set_speed(200 + speedk2);
 	}
 
 
--- process_image.c
+++ process_image.c
@@ -8,10 +8,6 @@
 
 #include <process_image.h>
 
-
-static float distance_cm = 0;
-static uint16_t centre = 0;
-
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
 
@@ -26,7 +22,6 @@ static THD_FUNCTION(CaptureImage, arg) {
 	dcmi_enable_double_buffering();
 	dcmi_set_capture_mode(CAPTURE_ONE_SHOT);
 	dcmi_prepare();
-	uint32_t start = 0;
 
     while(1){
         //starts a capture
@@ -35,8 +30,6 @@ static THD_FUNCTION(CaptureImage, arg) {
 		wait_image_ready();
 		//signals an image has been captured
 		chBSemSignal(&image_ready_sem);
-		//chprintf((BaseSequentialStream *)&SDU1, "time = %d \n", chVTGetSystemTime()-start);
-		start = chVTGetSystemTime();
     }
 }
 
@@ -58,52 +51,7 @@ static THD_FUNCTION(ProcessImage, arg) {
 
 		uint8_t img_buff[640] = {0};
 		uint8_t* pointeur = &img_buff;
-		uint64_t moyenne = 0;
-		for(int i = 0; i<640;i++){
-			uint8_t temp1 = img_buff_ptr[2*i+1];
-			uint8_t temp2 = img_buff_ptr[2*i];
-			temp1 &= 0b11100000;
-			temp1 = temp1 >> 5;
-			temp2 &= 0b00000111;
-			temp2 = temp2 << 3;
-			pointeur[i] = temp1 + temp2;
-			moyenne += temp1 + temp2;
 		}
-		moyenne /= 640;
-		uint16_t limGauche = 0;
-		uint32_t largeur = 0;
-		uint8_t threshold = moyenne / 1.3;
-		uint32_t largeurMax = 0;
-		for(int i = 0; i<640;i++){
-			if(pointeur[i]<threshold && largeur == 0){
-				largeur = i;
-				limGauche = i;
-				//chprintf((BaseSequentialStream *)&SDU1, "limGauche = %d \n", limGauche);
-			}
-			if(pointeur[i]>threshold && largeur != 0){
-				largeur = i - largeur;
-				if(largeur > largeurMax){
-					largeurMax = largeur;
-					centre = (limGauche + i )/2;
-					//chprintf((BaseSequentialStream *)&SDU1, "centre = %d \n", get_centre());
-				}
-				largeur = 0;
-			}
-		}
-		//chprintf((BaseSequentialStream *)&SDU1, "largeurMax = %d \n", largeurMax);
-		distance_cm = largeurMax / 11.7;
-		//chprintf((BaseSequentialStream *)&SDU1, "distance = %f \n", distance_cm);
-		SendUint8ToComputer(pointeur, 640);
-		chThdSleepMilliseconds(100);
-		}
-}
-
-uint16_t get_centre(void){
-	return centre;
-}
-
-float get_distance_cm(void){
-	return distance_cm;
 }
 
 void process_image_start(void){
