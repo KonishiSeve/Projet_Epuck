c3848f58028789ea419e94d75ecdabf3158db3fc
Séverin Konishi <konishi.seve@gmail.com>
2022-05-07 08:41:11 +0200

    Détection + follow du feu
    pas encore de détection de feu vert

--- navigation.c
+++ navigation.c
@@ -8,6 +8,8 @@
 
 #define CENTRE_IMAGE 320
 #define KP_FEU 1
+
+#define TAILLE_FEU_ATTENTE 70
 //Thread pour éviter les obstacles <-> s'aligner avec le feu
 // sleep 200ms
 //envoie à clignotant
@@ -31,21 +33,18 @@ THD_FUNCTION(navigation_thd,arg) {
 	int16_t diffspeed = 0;
 
 	while(1) {
+
 		//Mode suivit de route
-		while(1/*get_taille_feu()==0*/) {
-			//chprintf((BaseSequentialStream *)&SD3, "taille: %d \r \n", get_taille_feu());
+		while(get_general_state()==0/*get_taille_feu()==0*/) {
 			clignoter = 0;
 			// éviter obstacle()
 			const int8_t Kp = 1;
 			int16_t proxLeft = get_prox(7) + get_prox(6);
 			int16_t proxRight = get_prox(0) + get_prox(1);
-
 			int16_t diff = proxLeft - proxRight;
 			diffspeed = diff*Kp;
-
 			left_motor_set_speed(400 + diffspeed);
 			right_motor_set_speed(400 - diffspeed);
-
 			if(diffspeed > 50){
 				clignoter = 1;
 			}else if(diffspeed < -50){
@@ -53,21 +52,30 @@ THD_FUNCTION(navigation_thd,arg) {
 			}
 			chThdSleepMilliseconds(100);
 		}
+		clignoter = 0;
 		set_rgb_led(LED4, 99,0,0);
 		set_rgb_led(LED6, 99,0,0);
 		set_led(LED5,2);
-		//Mode alignement avec le feu et attente du feu vert
-		while(0/*get_taille_feu() != 0*/) {
-			//chprintf((BaseSequentialStream *)&SD3, "centre: %d \r \n", get_centre_feu());
-				int16_t erreur = get_centre_feu() - CENTRE_IMAGE;
-				left_motor_set_speed(150 + KP_FEU*erreur);
-				right_motor_set_speed(150 - KP_FEU*erreur);
+
+		//Mode alignement avec le feu rouge
+		while(get_general_state()==1/*get_taille_feu() > TAILLE_FEU_ATTENTE && get_taille_feu() != 0*/) {
+			int16_t erreur = get_centre_feu() - CENTRE_IMAGE;
+			left_motor_set_speed(150 + KP_FEU*erreur);
+			right_motor_set_speed(150 - KP_FEU*erreur);
+			chThdSleepMilliseconds(100);
 		}
 		set_rgb_led(LED4, 0,0,0);
 		set_rgb_led(LED6, 0,0,0);
 		set_led(LED5,0);
+
+		/*
+		//attente du feu vert
+		while(get_taille_feu() != 0) {
+			chThdSleepMilliseconds(500);
+		}*/
 	}
 }
+
 void navigation_start(void) {
 	chThdCreateStatic(navigation_thd_wa, sizeof(navigation_thd_wa), NORMALPRIO, navigation_thd, NULL);
 }
--- process_image.c
+++ process_image.c
@@ -13,7 +13,8 @@
 
 #define NIGHT_THRESHOLD 20
 #define RED_THRESHOLD 20
-#define TAILLE_FEU_THRESHOLD 20
+#define TAILLE_FEU_THRESHOLD 150
+#define THRESHOLD_GREEN 20
 
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
@@ -49,6 +50,11 @@ uint16_t get_taille_feu(void) {
 	return taille_feu;
 }
 
+//0: route, 1:suivit feu, 2:attente feu
+static uint8_t general_state = 0;
+uint8_t get_general_state(void) {
+	return general_state;
+}
 
 
 static THD_WORKING_AREA(waProcessImage, 1024);
@@ -61,6 +67,9 @@ static THD_FUNCTION(ProcessImage, arg) {
 	uint8_t image[IMAGE_BUFFER_SIZE] = {0};
 
 	uint8_t trigger_red = 0;
+	uint16_t mean_green_old = 0;
+
+	uint16_t debug_counter = 0;
 
     while(1){
     	//waits until an image has been captured
@@ -71,33 +80,29 @@ static THD_FUNCTION(ProcessImage, arg) {
 		uint8_t img_buff[640] = {0};
 		uint8_t* pointeur_image = &image;
 
-
+		//-------------------------CALCULS----------------------------
 		uint64_t moyenne_red = 0;
 		uint64_t moyenne_green = 0;
 		uint64_t moyenne_blue = 0;
-
 		for(int i = 0; i<640;i++){
 			uint8_t pixel_low = img_buff_ptr[2*i+1];
 			uint8_t pixel_high = img_buff_ptr[2*i];
-
 			uint8_t pixel_blue = pixel_low & 0b00011111;
 			uint8_t pixel_green = (pixel_low >> 5) + ((pixel_high & 0b00000111) << 3);
 			uint8_t pixel_red = pixel_high >> 3;
 			pointeur_image[i] = pixel_red;
-
-
 			moyenne_red += pixel_red;
-			moyenne_green += pixel_green;
 			moyenne_blue += pixel_blue;
+			moyenne_green += pixel_green;
 
 		}
 		moyenne_red = (moyenne_red/640) << 1; //conversion en 6 bits
 		moyenne_green = (moyenne_green/640);
 		moyenne_blue = (moyenne_blue/640) << 1; //conversion en 6 bits
 
-		//Détection de pic pour le feux rouge
+
+		//------------------Détection de pic pour le feux rouge--------------------------------
 		uint16_t threshold_red = moyenne_red/1.3;
-		//chprintf((BaseSequentialStream *)&SD3, "moyenne_rouge: %d", moyenne_red);
 		uint16_t largeur_pic = 0;
 		uint16_t limite_gauche_pic = 0;
 		uint16_t largeur_max = 0;
@@ -110,7 +115,6 @@ static THD_FUNCTION(ProcessImage, arg) {
 			}
 			if(pointeur_image[i] < threshold_red && largeur_pic !=0 ) {
 				largeur_pic = i-largeur_pic;
-				//chprintf((BaseSequentialStream *)&SD3, " fin du pic %d", largeur_pic);
 				if(largeur_pic > largeur_max) {
 					largeur_max = largeur_pic;
 					centre_pic = (limite_gauche_pic+i)/2;
@@ -118,11 +122,9 @@ static THD_FUNCTION(ProcessImage, arg) {
 				largeur_pic = 0;
 			}
 		}
+
+		//calcul de l'écart type seulement dans la région du pic rouge
 		uint64_t standev = 0;
-		/*
-		chprintf((BaseSequentialStream *)&SD3, " , limite gauche: %d", limite_gauche_pic);
-		chprintf((BaseSequentialStream *)&SD3, " , limite droite: %d \r \n", limite_gauche_pic+largeur_pic);
-		*/
 		for(int i=limite_gauche_pic; i<(limite_gauche_pic+largeur_max); i++) {
 			standev += abs(pointeur_image[i]-moyenne_red);
 		}
@@ -133,22 +135,51 @@ static THD_FUNCTION(ProcessImage, arg) {
 		chprintf((BaseSequentialStream *)&SD3, "taille: %d", taille_feu);
 		chprintf((BaseSequentialStream *)&SD3, " , centre: %d", centre_feu);
 		chprintf((BaseSequentialStream *)&SD3, " , moyenne: %d", moyenne_red);
+		chprintf((BaseSequentialStream *)&SD3, " , moyenne vert: %d", moyenne_green);
 		chprintf((BaseSequentialStream *)&SD3, " , std: %d \r \n", standev);
 
-		if(standev < 7 && standev > 0 && moyenne_red >= 20) {
+
+		//------------------------- GESTION DU STATE ET ENVOI DES VARIABLES --------------------------------
+		if(standev < 10 && standev > 0 && moyenne_red >= 20 && trigger_red < 3 && general_state == 0) {
+			trigger_red++;
+		} else if(trigger_red>=3 && general_state==0) {
+			general_state = 1;
+			trigger_red = 0;
+			chprintf((BaseSequentialStream *)&SD3, "----- RED TRIGGER-----");
+		} else {
+			trigger_red = 0;
+		}
+
+		if(general_state == 1) {
+			debug_counter++;
+		}
+
+		if(debug_counter >= 40) {
+			general_state = 0;
+			debug_counter = 0;
+		}
+
+		/*
+		if(standev < 10 && standev >= 0 && moyenne_red >= 20 && trigger_red < 3) {
 			trigger_red++;
 		}
 
-		if(trigger_red >=3) {
-			set_rgb_led(LED4, 99,0,0);
-			set_rgb_led(LED6, 99,0,0);
-			set_led(LED5,2);
+		if(moyenne_green > mean_green_old+5 && trigger_red >=4) {
+			taille_feu = 0;
+			centre_feu = 0;
 			trigger_red = 0;
-			chThdSleepMilliseconds(1000);
-			set_rgb_led(LED4, 0,0,0);
-			set_rgb_led(LED6, 0,0,0);
-			set_led(LED5,0);
-		}
+		} else if(trigger_red >=3) {
+			taille_feu = largeur_max;
+			centre_feu = centre_pic;
+			mean_green_old = moyenne_green;
+			chprintf((BaseSequentialStream *)&SD3, "RED TRIGGER");
+			trigger_red++;
+
+		} else {
+			taille_feu = 0;
+			centre_feu = 0;
+		}*/
+
 
 		if((moyenne_green + moyenne_blue < NIGHT_THRESHOLD - 5)){
 			//set_front_led(1);
@@ -156,11 +187,6 @@ static THD_FUNCTION(ProcessImage, arg) {
 			set_front_led(0);
 		}
 		chThdSleepMilliseconds(100);
-
-		/*if(feu) {
-			suit le feu()
-    	}*/
-
     }
 }
 
